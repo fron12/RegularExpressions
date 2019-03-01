337ea0cdcabecb8beccf3cf2574756999f969f7a
Fred <sohnf90@yahoo.com>
2019-02-28 16:56:05 -0800

    Reusing patterns for capture groups

--- CaptureGroups.js
+++ CaptureGroups.js
@@ -4,4 +4,10 @@ let result = wrongText.replace(redRegex, "blue");
 // console.log(result);
 
 let myDog = "My dog Rocky".replace(/(\w+)\s(\w+)\s(\w+)/, "$3 $2 $1");
-console.log(myDog);
+// console.log(myDog);
+
+let repeatWord = "Salad Salad Salad";
+let repeatRegex = /(\w+)\s\1\s\1/;
+let regexTest = repeatRegex.test(repeatWord);
+let regexMatch = repeatWord.match(repeatRegex);
+console.log(regexMatch);
