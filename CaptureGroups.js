465acf5bef109547c0f1b256217a85ae273c5748
Fred <sohnf90@yahoo.com>
2019-02-28 16:29:37 -0800

    Rearranging string using $

--- CaptureGroups.js
+++ CaptureGroups.js
@@ -1,4 +1,7 @@
 let wrongText = "The ocean is red.";
 let redRegex = /red/;
 let result = wrongText.replace(redRegex, "blue");
-console.log(result);
+// console.log(result);
+
+let myDog = "My dog Rocky".replace(/(\w+)\s(\w+)\s(\w+)/, "$3 $2 $1");
+console.log(myDog);
